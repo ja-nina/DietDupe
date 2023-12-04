2bd40fcae751813062737b0fc5b063bb7ca0a3da
ja-nina <zukowskanina1@gmail.com>
2023-12-04 14:17:16 +0100

    changed food embedder name

--- preprocessing/embedding_models/foodbert_embedder.py
+++ preprocessing/embedding_models/foodbert_embedder.py
@@ -3,7 +3,7 @@ from transformers import BertModel, BertTokenizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
-class BertEmbedder(BaseEmbedder):
+class FoodBertEmbedder(BaseEmbedder):
     def __init__(self):
         super().__init__()
         self.model = BertModel.from_pretrained('chambliss/distilbert-for-food-extraction')
@@ -17,7 +17,7 @@ class BertEmbedder(BaseEmbedder):
         return outputs.hidden_states[-1].detach().cpu().numpy()[0, 0, :].flatten()
     
     
-class BertEmbedderAvg(BertEmbedder):
+class FoodBertEmbedderAvg(FoodBertEmbedder):
     def __init__(self):
         super().__init__()
     
@@ -27,7 +27,7 @@ class BertEmbedderAvg(BertEmbedder):
         return outputs[0].detach().cpu().numpy().squeeze().mean(axis=0)
     
 if __name__=="__main__":
-    bert_embedder = BertEmbedder()
+    bert_embedder = FoodBertEmbedder()
     embed1 = bert_embedder.embed("BUTTERMILK PANCAKES BANANA FLAVOUR")
     embed2 = bert_embedder.embed("BUTTER WHOLE FAT")
     embed3 = bert_embedder.embed("BUTTERMILK")
@@ -35,7 +35,7 @@ if __name__=="__main__":
     similarity_matrix = cosine_similarity([embed1, embed2, embed3, embed4], [embed1, embed2, embed3, embed4])
     print(similarity_matrix)
     
-    bert_embedder = BertEmbedderAvg()
+    bert_embedder = FoodBertEmbedderAvg()
     embed1 = bert_embedder.embed("BUTTERMILK PANCAKES BANANA FLAVOUR")
     embed2 = bert_embedder.embed("BUTTER WHOLE FAT")
     embed3 = bert_embedder.embed("BUTTERMILK")

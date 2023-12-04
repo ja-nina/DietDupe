9df754bb8ae0c003ff3e75a752baf714d81040f5
ja-nina <zukowskanina1@gmail.com>
2023-12-04 14:05:26 +0100

    foodbert from huggingface

--- preprocessing/embedding_models/foodbert_embedder.py
+++ preprocessing/embedding_models/foodbert_embedder.py
@@ -1,40 +1,44 @@
-#!/usr/bin/env python3
-import sys
-sys.path.append("../../")
-from foodbert.food_extractor.food_model import FoodModel
 from preprocessing.embedding_models.base_embedder import BaseEmbedder
+from transformers import BertModel, BertTokenizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
-class FoodBertEmbedder(BaseEmbedder):
-    """
-    FoodBERT: Food Extraction with DistilBERT
-    <https://github.com/chambliss/foodbert>
-    """
-    def __init__(self,):
+class BertEmbedder(BaseEmbedder):
+    def __init__(self):
         super().__init__()
-        self.model = FoodModel("chambliss/distilbert-for-food-extraction")
+        self.model = BertModel.from_pretrained('chambliss/distilbert-for-food-extraction')
+        self.tokenizer = BertTokenizer.from_pretrained('distilbert')
+        self.post_init()
+        print("SELF DEVICE", self.device)
 
     def _embed(self, text: str) -> np.ndarray:
-        try:
-            inputs = self.model.tokenizer(
-                text,
-                padding=True,
-                truncation=True,
-                add_special_tokens=False,
-                return_tensors="pt",
-            ).to(self.device)
-            outputs = self.model.model.forward(inputs["input_ids"], output_hidden_states=True)
-            return outputs.hidden_states[-1].detach().cpu().numpy()[0, 0, :].flatten()
-        except ValueError:
-            return np.zeros(300)
-
-
-if __name__ == "__main__":
-    foodbert_embedder = FoodBertEmbedder()
-    embed1 = foodbert_embedder.embed("BUTTERMILK PANCAKES BANANA FLAVOUR")
-    embed2 = foodbert_embedder.embed("BUTTER WHOLE FAT")
-    embed3 = foodbert_embedder.embed("BUTTERMILK")
-    embed4 = foodbert_embedder.embed("PANCAKES MADE FROM BUTTERMILK")
+        inputs = self.tokenizer(text, return_tensors='pt').to(self.device)
+        outputs = self.model(**inputs, output_hidden_states=True)
+        return outputs.hidden_states[-1].detach().cpu().numpy()[0, 0, :].flatten()
+    
+    
+class BertEmbedderAvg(BertEmbedder):
+    def __init__(self):
+        super().__init__()
+    
+    def _embed(self, text: str) -> np.ndarray:
+        inputs = self.tokenizer(text, return_tensors='pt').to(self.device)
+        outputs = self.model(**inputs)
+        return outputs[0].detach().cpu().numpy().squeeze().mean(axis=0)
+    
+if __name__=="__main__":
+    bert_embedder = BertEmbedder()
+    embed1 = bert_embedder.embed("BUTTERMILK PANCAKES BANANA FLAVOUR")
+    embed2 = bert_embedder.embed("BUTTER WHOLE FAT")
+    embed3 = bert_embedder.embed("BUTTERMILK")
+    embed4 = bert_embedder.embed("PANCAKES MADE FROM BUTTERMILK")
+    similarity_matrix = cosine_similarity([embed1, embed2, embed3, embed4], [embed1, embed2, embed3, embed4])
+    print(similarity_matrix)
+    
+    bert_embedder = BertEmbedderAvg()
+    embed1 = bert_embedder.embed("BUTTERMILK PANCAKES BANANA FLAVOUR")
+    embed2 = bert_embedder.embed("BUTTER WHOLE FAT")
+    embed3 = bert_embedder.embed("BUTTERMILK")
+    embed4 = bert_embedder.embed("PANCAKES MADE FROM BUTTERMILK")
     similarity_matrix = cosine_similarity([embed1, embed2, embed3, embed4], [embed1, embed2, embed3, embed4])
     print(similarity_matrix)

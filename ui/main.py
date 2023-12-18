844ffd84d21ffcc587c2fc6f938a6093771cd0cd
Antoni Solarski <antoni.solarski@tomtom.com>
2023-12-18 14:15:31 +0100

    wip

--- ui/main.py
+++ ui/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import streamlit as st
 
-from retrieval.retriever import DietDupe
+from dietdupe.retrieval.diet_dupe import DietDupe
+from dietdupe.retrieval import Nutrient, RecipeRestriction
 
 
 def get_products():
@@ -14,14 +15,7 @@ def get_products():
 
 def get_categories():
     categories = [
-        "Water_(g)", "Energ_Kcal", "Protein_(g)", "Lipid_Tot_(g)", "Ash_(g)", "Carbohydrt_(g)", "Fiber_TD_(g)",
-        "Sugar_Tot_(g)", "Calcium_(mg)", "Iron_(mg)", "Magnesium_(mg)", "Phosphorus_(mg)", "Potassium_(mg)",
-        "Sodium_(mg)", "Zinc_(mg)", "Copper_mg)", "Manganese_(mg)", "Selenium_(µg)", "Vit_C_(mg)",
-        "Thiamin_(mg)", "Riboflavin_(mg)", "Niacin_(mg)", "Panto_Acid_mg)", "Vit_B6_(mg)", "Folate_Tot_(µg)",
-        "Folic_Acid_(µg)", "Food_Folate_(µg)", "Folate_DFE_(µg)", "Choline_Tot_ (mg)", "Vit_B12_(µg)", "Vit_A_IU",
-        "Vit_A_RAE", "Retinol_(µg)", "Alpha_Carot_(µg)", "Beta_Carot_(µg)", "Beta_Crypt_(µg)", "Lycopene_(µg)",
-        "Lut+Zea_ (µg)", "Vit_E_(mg)", "Vit_D_µg", "Vit_D_IU", "Vit_K_(µg)", "FA_Sat_(g)", "FA_Mono_(g)",
-        "FA_Poly_(g)", "Cholestrl_(mg)"
+        "Water", "Protein"
     ]
     return categories
 
@@ -54,11 +48,13 @@ def format_selected_products(matched_data, nutritional_data, selected_products, 
 def propose_products(diet_dupe: DietDupe, products, nutritional_data, selected_product, selected_restrictions):
     product_id = products[products['name'] == selected_product]['node_id'].tolist()
     # FIXME:
+    print(product_id)
     # proposal_names = diet_dupe.run(product_id, [(k, v) for k, v in selected_restrictions.items()])
-    proposal_names = ["TOFU,FRIED", "CHEESE,CAMEMBERT"]
+    proposal_names = diet_dupe.run(product_id, [RecipeRestriction(category=v, nutrient=Nutrient[k]) for k, v in selected_restrictions.items()])
+    # proposal_names = ["TOFU,FRIED", "CHEESE,CAMEMBERT"]
     proposal = pd.DataFrame({'name': proposal_names})
-    for r in selected_restrictions:
-        proposal[r] = [nutritional_data[nutritional_data['Shrt_Desc'] == p][r].tolist()[0] for p in proposal_names]
+    # for r in selected_restrictions:
+    #     proposal[r] = [nutritional_data[nutritional_data['Shrt_Desc'] == p][r].tolist()[0] for p in proposal_names]
     return proposal
 
 
@@ -88,8 +84,9 @@ def main():
         selected_restrictions[category] = restraint
 
     st.subheader('Selected Products:')
-    st.write(format_selected_products(
-        matched_nutritional_data, nutritional_data, selected_products, selected_restrictions))
+    st.write(selected_products)
+    # st.write(format_selected_products(
+    #     matched_nutritional_data, nutritional_data, selected_products, selected_restrictions))
 
     if st.sidebar.button('Confirm Selection'):
         for selected_product in selected_products:
